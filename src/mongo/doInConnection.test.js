e1c1ea7752489a9f7ff03d821494e97aaa78d6b3
quidcain <quidcain@gmail.com>
2020-08-05 09:45:33 +0300

    Enhance doInConnection test

--- src/mongo/doInConnection.test.js
+++ src/mongo/doInConnection.test.js
@@ -1,13 +1,8 @@
 const doInConnection = require('./doInConnection');
 
 test('doInConnection', async () => {
-  try {
-    await doInConnection(async db => {
-      await db.command({ ping: 1 });
-    });
-    expect(true).toBe(true);
-  } catch(e) {
-    console.log(e);
-    expect(true).toBe(false);
-  }
+  const promise = doInConnection(async db => {
+    return await db.command({ ping: 1 });
+  });
+  return expect(promise).resolves.toEqual({ ok: 1 });
 });
